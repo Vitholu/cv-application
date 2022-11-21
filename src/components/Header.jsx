2daacc660db56461e263f15473e4a17ca5361818
Vitholu <116558222+Vitholu@users.noreply.github.com>
2022-11-21 14:46:16 +0100

    Add edit feature for name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,82 @@ import React, { Component } from 'react'
 
 
 export class Header extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      isActive: false,
+      
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleActive = this.toggleActive.bind(this);
+
+  }
+
+  handleChange(event) {
+    if (event !== undefined) {
+      this.setState({value: event.target.value});
+    }
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    this.setState({isActive: false})
+    event.preventDefault();
+  }
+  
+  toggleActive() {
+    if (!(this.state.isActive)) {
+      return this.setState({isActive: true})
+    }
+  }
+
+  activeContent() {
     return (
-			<div className='header'>
-				<div className='header-left'>
-					<h1>CC</h1>
-					<div className='header-left-wrapper'>
-						<h2>Chris Candidate</h2>
-						<h4>Human Resource Manager</h4>
+			<label>
+				<input
+					type='text'
+					value={this.state.value}
+					onChange={this.handleChange}
+				/>
+			</label>
+		);
+  }
+
+
+  inActiveContent() {
+    return (
+			<h2>{this.state.value || "Chris Candidate"}</h2>
+		);
+  }
+
+  
+  
+  render() {
+    const inActiveContent = this.inActiveContent();
+    const activeContent = this.activeContent();
+
+
+    return (
+			<form onSubmit={this.handleSubmit}>
+				<div className='header'>
+					<div className='header-left'>
+						<h1>CC</h1>
+						<div className='header-left-wrapper'>
+							<h3 onClick={this.toggleActive}>{this.state.isActive ? activeContent : inActiveContent}</h3>
+							<h4>Human Resource Manager</h4>
+						</div>
+					</div>
+					<div className='header-right'>
+						<p>
+							4759 Sunnydale Lane Plano, TX, 75071 email@youremail.com (469)
+							385-2948
+						</p>
 					</div>
 				</div>
-				<div className='header-right'>
-					<p>
-						4759 Sunnydale Lane Plano, TX, 75071 email@youremail.com (469)
-						385-2948
-					</p>
-				</div>
-			</div>
+			</form>
 		);
   }
 }
