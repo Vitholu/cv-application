e1c16c83ed5a1020b05c9e44873abda4f2a4a21a
Vitholu <116558222+Vitholu@users.noreply.github.com>
2022-11-23 13:06:39 +0100

    add professional experience section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,6 +27,12 @@ export class Header extends Component {
         four: false,
         textD: "",
       },
+      fifth: {
+        title: "Human Resources Manager",
+        experience: false,
+        five: false,
+        textE: "",
+      },
       value: ''
 		};
 
@@ -54,6 +60,10 @@ export class Header extends Component {
       case '4':
         this.setState({ textD: event.target.value });
         break;  
+      
+      case '5':
+      this.setState({ textE: event.target.value });
+      break;  
 
 
 			default:
@@ -73,8 +83,13 @@ export class Header extends Component {
     this.setState({ address: false });
     this.setState({ three: false });
 
+
     this.setState({ description: false })
     this.setState({ four: false })
+
+
+    this.setState({ experience: false })
+    this.setState({ five: false })
 
 		event.preventDefault();
 	}
@@ -100,6 +115,11 @@ export class Header extends Component {
       case '4':
         this.setState({ description: true })
         this.setState({ four: true })
+      break;
+
+      case '5':
+        this.setState({ experience: true })
+        this.setState({ five: true })
       break;
 
 			default:
@@ -134,7 +154,7 @@ export class Header extends Component {
             <input
               className={key}
               type='text'
-              textA={this.state.value}
+              textC={this.state.value}
               onChange={this.handleChange}
             />
           </label>)
@@ -144,7 +164,17 @@ export class Header extends Component {
             <input
               className={key}
               type='text'
-              textA={this.state.value}
+              textD={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>)
+        
+        case "5":
+          return (<label>
+            <input
+              className={key}
+              type='text'
+              textE={this.state.value}
               onChange={this.handleChange}
             />
           </label>)
@@ -168,6 +198,33 @@ export class Header extends Component {
       case "4":
         return <p className='4'>{this.state.textD || "Human resources generalist with 8 years of experience in HR, including hiring and terminating, disciplining employees and helping department managers improve employee performance. Worked with labor unions to negotiate compensation packages for workers. Organized new hire training initiatives as well as ongoing training to adhere to workplace safety standards. Worked with OSHA to ensure that all safety regulations are followed." }</p>;
       
+      case "5":
+      return (
+        <>
+        <div className="container-exp">
+
+          <div className="left-exp">
+            <p> January 2016 - Present </p>
+          </div>
+
+          <div className="right-exp">
+
+            <h4 className="exp-title">{this.state.fifth.title}</h4>
+            <ul>
+              <li className='5'>
+                {this.state.textE || "Implement effective company policies to ensure that all practices comply with labor and employment regulations"}
+              </li>
+              <li className='5'>
+                {this.state.textE || "Implement effective company policies to ensure that all practices comply with labor and employment regulations"}
+              </li>
+              <li className='5'>
+                {this.state.textE || "Implement effective company policies to ensure that all practices comply with labor and employment regulations"}
+              </li>
+            </ul>
+          </div>
+        </div>
+        </>
+      )
       default:
         break;
     }
@@ -209,6 +266,19 @@ export class Header extends Component {
                   {this.state.description ? this.TextInput("4") : this._NameContent("4")}
                 </p>
           </form>
+      </div>
+
+
+
+      <div className="bg-experience">
+        <h3>
+          Professional Experience
+        </h3>
+        <form onSubmit={this.handleSubmit}>
+              <p className='5 experience' onClick={this.toggleActive}>
+                {this.state.experience ? this.TextInput("5") : this._NameContent("5")}
+              </p>
+        </form>
       </div>
       </>
 		);
